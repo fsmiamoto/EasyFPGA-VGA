fb0c88c5a146d95c0e1e688b430106e806a486e2
Francisco Miamoto <fsmiamoto@gmail.com>
2021-04-12 11:53:43 -0300

    refactor: improve naming

--- VgaController.vhd
+++ VgaController.vhd
@@ -9,48 +9,50 @@ use ieee.numeric_std.all;
 entity VgaController is
   port (
     clk   : in std_logic; -- Pin 23, the clock provided by the board is 50Mhz
-    rgb   : out std_logic_vector (2 downto 0); -- Pins 106,105 and 104
+    rgb   : out std_logic_vector (2 downto 0); -- Pins 106, 105 and 104
     hsync : out std_logic; -- Pin 101
     vsync : out std_logic -- Pin 103
   );
 end VgaController;
 
 architecture rtl of VgaController is
-  constant WHITE  : std_logic_vector := "111";
-  constant YELLOW : std_logic_vector := "110";
-  constant PURPLE : std_logic_vector := "101";
-  constant RED    : std_logic_vector := "100";
-  constant WATER  : std_logic_vector := "011";
-  constant GREEN  : std_logic_vector := "010";
-  constant BLUE   : std_logic_vector := "001";
-  constant BLACK  : std_logic_vector := "000";
+  constant COLOR_WHITE  : std_logic_vector := "111";
+  constant COLOR_YELLOW : std_logic_vector := "110";
+  constant COLOR_PURPLE : std_logic_vector := "101";
+  constant COLOR_RED    : std_logic_vector := "100";
+  constant COLOR_WATER  : std_logic_vector := "011";
+  constant COLOR_GREEN  : std_logic_vector := "010";
+  constant COLOR_BLUE   : std_logic_vector := "001";
+  constant COLOR_BLACK  : std_logic_vector := "000";
 
-  constant HSYNC_END  : integer := 95;
-  constant HDAT_BEGIN : integer := 143;
-  constant HDAT_END   : integer := 783;
-  constant HLINE_END  : integer := 799;
-  constant VSYNC_END  : integer := 1;
-  constant VDAT_BEGIN : integer := 34;
-  constant VDAT_END   : integer := 514;
-  constant VLINE_END  : integer := 524;
+  -- Values for 640x480 resolution
+  constant HSYNC_END   : integer := 95;
+  constant HDATA_BEGIN : integer := 143;
+  constant HDATA_END   : integer := 783;
+  constant HLINE_END   : integer := 799;
 
-  signal hcount         : integer := 0;
-  signal vcount         : integer := 0;
-  signal vcount_ov      : boolean;
-  signal hcount_ov      : boolean;
-  signal data           : std_logic_vector (2 downto 0);
-  signal is_data_active : boolean;
-  signal vga_clk        : std_logic := '0';
+  constant VSYNC_END   : integer := 1;
+  constant VDATA_BEGIN : integer := 34;
+  constant VDATA_END   : integer := 514;
+  constant VLINE_END   : integer := 524;
 
+  signal hcount : integer := 0;
+  signal vcount : integer := 0;
+  signal data   : std_logic_vector (2 downto 0);
+
+  signal vga_clk : std_logic := '0';
+
+  signal should_reset_vcount : boolean;
+  signal should_reset_hcount : boolean;
+  signal should_output_data  : boolean;
 begin
-  vcount_ov      <= vcount = VLINE_END;
-  hcount_ov      <= hcount = HLINE_END;
-  is_data_active <= (hcount >= HDAT_BEGIN) and (hcount < HDAT_END) and (vcount >= VDAT_BEGIN) and (vcount < VDAT_END);
+  should_reset_vcount <= vcount = VLINE_END;
+  should_reset_hcount <= hcount = HLINE_END;
+  should_output_data  <= (hcount >= HDATA_BEGIN) and (hcount < HDATA_END) and (vcount >= VDATA_BEGIN) and (vcount < VDATA_END);
 
-  -- Outputs
   hsync <= '1' when hcount > HSYNC_END else '0';
   vsync <= '1' when vcount > VSYNC_END else '0';
-  rgb   <= data when is_data_active else (others => '0');
+  rgb   <= data when should_output_data else (others => '0');
 
   -- We need 25MHz for the VGA so we divide the input clock by 2
   process (clk)
@@ -63,7 +65,7 @@ begin
   process (vga_clk)
   begin
     if (rising_edge(vga_clk)) then
-      if (hcount_ov) then
+      if (should_reset_hcount) then
         hcount <= 0;
       else
         hcount <= hcount + 1;
@@ -73,8 +75,8 @@ begin
 
   process (vga_clk)
   begin
-    if (rising_edge(vga_clk) and hcount_ov) then
-      if (vcount_ov) then
+    if (rising_edge(vga_clk) and should_reset_hcount) then
+      if (should_reset_vcount) then
         vcount <= 0;
       else
         vcount <= vcount + 1;
@@ -86,21 +88,21 @@ begin
   begin
     if (rising_edge(vga_clk)) then
       if (hcount < 223) then
-        data <= WHITE;
+        data <= COLOR_WHITE;
       elsif (hcount < 303) then
-        data <= YELLOW;
+        data <= COLOR_YELLOW;
       elsif (hcount < 383) then
-        data <= BLUE;
+        data <= COLOR_BLUE;
       elsif (hcount < 463) then
-        data <= GREEN;
+        data <= COLOR_GREEN;
       elsif (hcount < 543) then
-        data <= PURPLE;
+        data <= COLOR_PURPLE;
       elsif (hcount < 623) then
-        data <= RED;
+        data <= COLOR_RED;
       elsif (hcount < 703) then
-        data <= WATER;
+        data <= COLOR_WATER;
       else
-        data <= BLACK;
+        data <= COLOR_BLACK;
       end if;
     end if;
   end process;
