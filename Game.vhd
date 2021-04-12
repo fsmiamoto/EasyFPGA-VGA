ab4333188accc6c3631246106b9e11828fdb7802
Francisco Miamoto <fsmiamoto@gmail.com>
2021-04-12 16:02:50 -0300

    feat: move drawing logic to game entity

---
+++ Game.vhd
@@ -0,0 +1,98 @@
+library ieee;
+use ieee.std_logic_1164.all;
+use ieee.numeric_std.all;
+use work.vga_utils.all;
+
+entity Game is
+  port (
+    clk   : in std_logic; -- Pin 23
+    rgb   : out std_logic_vector (2 downto 0); -- Pins 106, 105 and 104
+    hsync : out std_logic; -- Pin 101
+    vsync : out std_logic; -- Pin 103
+    up    : in std_logic;
+    down  : in std_logic;
+    left  : in std_logic;
+    right : in std_logic
+  );
+end entity Game;
+
+architecture rtl of Game is
+  constant COLOR_WHITE  : std_logic_vector := "111";
+  constant COLOR_YELLOW : std_logic_vector := "110";
+  constant COLOR_PURPLE : std_logic_vector := "101";
+  constant COLOR_RED    : std_logic_vector := "100";
+  constant COLOR_WATER  : std_logic_vector := "011";
+  constant COLOR_GREEN  : std_logic_vector := "010";
+  constant COLOR_BLUE   : std_logic_vector := "001";
+  constant COLOR_BLACK  : std_logic_vector := "000";
+
+  constant HDATA_BEGIN : integer := 143;
+  constant H_EIGHTH    : integer := 640 / 8;
+  constant H_HALF      : integer := 640 / 2;
+  constant H_QUARTER   : integer := 640 / 4;
+
+  constant VDATA_BEGIN : integer := 34;
+  constant V_EIGHTH    : integer := 480 / 8;
+  constant V_HALF      : integer := 480 / 2;
+  constant V_QUARTER   : integer := 480 / 4;
+
+  signal vga_clk : std_logic;
+
+  signal rgb_input, rgb_output : std_logic_vector(2 downto 0);
+  signal vga_hsync, vga_vsync  : std_logic;
+  signal hpos, vpos            : integer;
+
+  signal square_size : integer := 50;
+  signal square_x    : integer := HDATA_BEGIN + H_HALF - square_size/2;
+  signal square_y    : integer := VDATA_BEGIN + V_HALF - square_size/2;
+
+  signal should_draw_square : boolean;
+
+  component VgaController is
+    port (
+      clk     : in std_logic;
+      rgb_in  : in std_logic_vector (2 downto 0);
+      rgb_out : out std_logic_vector (2 downto 0);
+      hsync   : out std_logic;
+      vsync   : out std_logic;
+      hpos    : out integer;
+      vpos    : out integer
+    );
+  end component;
+
+begin
+  controller : VgaController port map(
+    clk     => vga_clk,
+    rgb_in  => rgb_input,
+    rgb_out => rgb_output,
+    hsync   => vga_hsync,
+    vsync   => vga_vsync,
+    hpos    => hpos,
+    vpos    => vpos
+  );
+
+  rgb   <= rgb_output;
+  hsync <= vga_hsync;
+  vsync <= vga_vsync;
+
+  Square(hpos, vpos, square_x, square_y, square_size, should_draw_square);
+
+  -- We need 25MHz for the VGA so we divide the input clock by 2
+  process (clk)
+  begin
+    if (rising_edge(clk)) then
+      vga_clk <= not vga_clk;
+    end if;
+  end process;
+
+  process (vga_clk)
+  begin
+    if (rising_edge(vga_clk)) then
+      if (should_draw_square) then
+        rgb_input <= COLOR_BLUE;
+      else
+        rgb_input <= COLOR_RED;
+      end if;
+    end if;
+  end process;
+end architecture;
--- VgaController.vhd
+++ VgaController.vhd
@@ -5,27 +5,20 @@
 library ieee;
 use ieee.std_logic_1164.all;
 use ieee.numeric_std.all;
-use work.vga_utils.all;
 
 entity VgaController is
   port (
-    clk   : in std_logic; -- Pin 23, the clock provided by the board is 50Mhz
-    rgb   : out std_logic_vector (2 downto 0); -- Pins 106, 105 and 104
-    hsync : out std_logic; -- Pin 101
-    vsync : out std_logic -- Pin 103
+    clk     : in std_logic;
+    rgb_in  : in std_logic_vector (2 downto 0);
+    rgb_out : out std_logic_vector (2 downto 0);
+    hsync   : out std_logic;
+    vsync   : out std_logic;
+    hpos    : out integer;
+    vpos    : out integer
   );
 end VgaController;
 
 architecture rtl of VgaController is
-  constant COLOR_WHITE  : std_logic_vector := "111";
-  constant COLOR_YELLOW : std_logic_vector := "110";
-  constant COLOR_PURPLE : std_logic_vector := "101";
-  constant COLOR_RED    : std_logic_vector := "100";
-  constant COLOR_WATER  : std_logic_vector := "011";
-  constant COLOR_GREEN  : std_logic_vector := "010";
-  constant COLOR_BLUE   : std_logic_vector := "001";
-  constant COLOR_BLACK  : std_logic_vector := "000";
-
   -- Values for 640x480 resolution
   constant HSYNC_END   : integer := 95;
   constant HDATA_BEGIN : integer := 143;
@@ -37,56 +30,27 @@ architecture rtl of VgaController is
   constant VDATA_END   : integer := 514;
   constant VLINE_END   : integer := 524;
 
-  constant H_EIGHTH  : integer := 640 / 8;
-  constant H_HALF    : integer := 640 / 2;
-  constant H_QUARTER : integer := 640 / 4;
-
-  constant V_EIGHTH  : integer := 480 / 8;
-  constant V_HALF    : integer := 480 / 2;
-  constant V_QUARTER : integer := 480 / 4;
-
-  -- Signals
   signal hcount : integer range 0 to HLINE_END := 0;
   signal vcount : integer range 0 to VLINE_END := 0;
-  signal data   : std_logic_vector (2 downto 0);
-
-  signal vga_clk : std_logic := '0';
 
   signal should_reset_vcount : boolean;
   signal should_reset_hcount : boolean;
   signal should_output_data  : boolean;
 
-  signal square_x           : integer range 0 to HLINE_END := 0;
-  signal square_y           : integer range 0 to VLINE_END := 0;
-  signal square_size        : integer                      := 50;
-  signal should_draw_square : boolean;
-
 begin
-  -- Middle of the screen;
-  square_x <= HDATA_BEGIN + H_HALF - square_size/2;
-  square_y <= VDATA_BEGIN + V_HALF - square_size/2;
-
-  square(hcount, vcount, square_x, square_y, square_size, should_draw_square);
-
   should_reset_vcount <= vcount = VLINE_END;
   should_reset_hcount <= hcount = HLINE_END;
   should_output_data  <= (hcount >= HDATA_BEGIN) and (hcount < HDATA_END) and (vcount >= VDATA_BEGIN) and (vcount < VDATA_END);
 
-  hsync <= '1' when hcount > HSYNC_END else '0';
-  vsync <= '1' when vcount > VSYNC_END else '0';
-  rgb   <= data when should_output_data else (others => '0');
+  hsync   <= '1' when hcount > HSYNC_END else '0';
+  vsync   <= '1' when vcount > VSYNC_END else '0';
+  rgb_out <= rgb_in when should_output_data else (others => '0');
+  hpos    <= hcount;
+  vpos    <= vcount;
 
-  -- We need 25MHz for the VGA so we divide the input clock by 2
   process (clk)
   begin
     if (rising_edge(clk)) then
-      vga_clk <= not vga_clk;
-    end if;
-  end process;
-
-  process (vga_clk)
-  begin
-    if (rising_edge(vga_clk)) then
       if (should_reset_hcount) then
         hcount <= 0;
       else
@@ -95,24 +59,13 @@ begin
     end if;
   end process;
 
-  process (vga_clk)
+  process (clk)
   begin
-    if (rising_edge(vga_clk) and should_reset_hcount) then
+    if (rising_edge(clk) and should_reset_hcount) then
       if (should_reset_vcount) then
         vcount <= 0;
       else
         vcount <= vcount + 1;
-      end if;
-    end if;
-  end process;
-
-  process (vga_clk)
-  begin
-    if (rising_edge(vga_clk)) then
-      if (should_draw_square) then
-        data <= COLOR_BLUE;
-      else
-        data <= COLOR_BLACK;
       end if;
     end if;
   end process;
